469afde627668d1caa7b86c9120c0552594b3857
erickaleg <erickaleg75@gmail.com>
2025-05-12 15:52:42 -0300

    Add files via upload

---
+++ codigo_de_fluxograma.py
@@ -0,0 +1,16 @@
+#saldo = int(input("digite o saldo bancario"))
+#saque = int(input("digite o valor de saque"))
+#if saldo >= saque:
+  #  saldo = saldo - saque 
+ #   print("voce realizou um saque com sucesso. Seu saldo é:", saldo)
+#else:
+        #print("você não possui saldo suficiente para realizar essa operação. Seu saldo é:" \
+        #"", saldo)
+
+nota1 =int(input("digite a primeira nota"))
+nota2 =int(input("digite a segunda nota"))
+media = (nota1+nota2)/2
+if media >= 6:
+    print("aprovado")
+else:
+    print("reprovado")
