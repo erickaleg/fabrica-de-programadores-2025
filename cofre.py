72a68134473dcf0e38f4f06604e3439b8509bbc6
erickaleg <erickaleg75@gmail.com>
2025-05-15 15:21:55 -0300

    aula do dia 14/15

---
+++ cofre.py
@@ -0,0 +1,32 @@
+#import senha
+
+#print(senha.minha_senha)
+
+#import pietra
+
+#print(pietra.CparaF)
+
+#import random
+
+#numero_aleatorio = random.random()
+#print(numero_aleatorio)
+
+#import random
+#numero_aleatorio = random.randint(0,10)
+#print(numero_aleatorio)
+
+#import random 
+
+#cesta = [ 'fortnite','free fire','among us','lol']
+#print(random.choice(cesta))
+
+#import time
+
+#print(time.asctime())
+
+#import turtle
+
+#turtle.circle(50)
+#turtle.getscreen()._root.mainloop()
+
+import this
---
+++ pietra.py
@@ -0,0 +1,15 @@
+def CparaF():
+   C = int(input("digite as temperatura em graus celsius: "))
+   F = (9*C+160)/5
+   conversão = str(F)
+   mcc = "a conversão para fahrenheit: " + conversão 
+   print(mcc)
+
+CparaF()
+
+num1 = int(input("digite o valor"))
+num2 = (int(input))
+if num1 > num2:
+  print('o resultado e: ,num1-num2')
+else:
+  print('o resultado e: ,num1-num2')
